01d737b71d5f8f697370879806c4f6c11fc80cfc
Kaushik Donthi <kaushikdr@gmail.com>
2022-04-12 19:31:00 -0700

    Change response and request HTTP functions to have more specific names

--- bxserum/provider/http.go
+++ bxserum/provider/http.go
@@ -32,17 +32,17 @@ func NewHTTPTestnet() *HTTPClient {
 
 // Connects to custom Serum API
 func NewHTTPClientWithEndpoint(baseURL string) *HTTPClient {
-	client := http.Client{Timeout: time.Second * 7}
+	client := http.Client{Timeout: time.Second * 7}           // TODO should we allow users to set the timeout?
 	return &HTTPClient{baseURL: baseURL, httpClient: &client} // TODO handle possible forward slash at end of base url?
 }
 
 func (w *HTTPClient) GetOrderbook(market string) (*pb.GetOrderbookResponse, error) {
 	url := w.baseURL + fmt.Sprintf("/api/v1/market/orderbooks/%s", market)
-	return getResponse[pb.GetOrderbookResponse](w.httpClient, url)
+	return getHTTPResponse[pb.GetOrderbookResponse](w.httpClient, url)
 }
 
 // response for GET request
-func getResponse[T any](client *http.Client, url string) (*T, error) {
+func getHTTPResponse[T any](client *http.Client, url string) (*T, error) {
 	if client == nil {
 		return nil, errors.New("client is nil, please create one using a `NewHTTPClient` function")
 	}
@@ -51,10 +51,10 @@ func getResponse[T any](client *http.Client, url string) (*T, error) {
 		return nil, err
 	}
 
-	return unmarshalResponse[T](httpResp)
+	return unmarshalHTTPResponse[T](httpResp)
 }
 
-func unmarshalResponse[T any](httpResp *http.Response) (*T, error) {
+func unmarshalHTTPResponse[T any](httpResp *http.Response) (*T, error) {
 	if httpResp == nil {
 		return nil, fmt.Errorf("HTTP response is nil")
 	}
