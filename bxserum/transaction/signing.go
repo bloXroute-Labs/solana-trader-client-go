7a994c3ae067c40d19506a09abf63d65b6c9e56b
Kaushik Donthi <kaushikdr@gmail.com>
2022-05-12 14:35:20 -0700

    Refactored SignTx to replace zero signature and created new function `SignWithPrivateKey`

--- bxserum/transaction/signing.go
+++ bxserum/transaction/signing.go
@@ -7,25 +7,34 @@ import (
 	"os"
 )
 
-// SignTx uses the environment variable for `PRIVATE_KEY` to sign the transaction
+// SignTx uses the environment variable for `PRIVATE_KEY` to sign the message content and replace the zero signature
 func SignTx(unsignedTxBase64 string) (string, error) {
-	pKeyStr, ok := os.LookupEnv("PRIVATE_KEY")
+	privateKeyBase58, ok := os.LookupEnv("PRIVATE_KEY")
 	if !ok {
 		return "", fmt.Errorf("env variable `PRIVATE_KEY` not set")
 	}
-	pKey, err := solana.PrivateKeyFromBase58(pKeyStr)
+
+	privateKey, err := solana.PrivateKeyFromBase58(privateKeyBase58)
 	if err != nil {
 		return "", err
 	}
 
+	return SignTxWithPrivateKey(unsignedTxBase64, privateKey)
+}
+
+// SignTxWithPrivateKey uses the provided private key to sign the message content and replace the zero signature
+func SignTxWithPrivateKey(unsignedTxBase64 string, privateKey solana.PrivateKey) (string, error) {
 	unsignedTxBytes, err := solanarpc.DataBytesOrJSONFromBase64(unsignedTxBase64)
 	if err != nil {
 		return "", err
 	}
 	unsignedTx := solanarpc.TransactionWithMeta{Transaction: unsignedTxBytes}
 	solanaTx, err := unsignedTx.GetTransaction()
+	if err != nil {
+		return "", err
+	}
 
-	err = signTx(solanaTx, pKey)
+	err = signTx(solanaTx, privateKey)
 	if err != nil {
 		return "", err
 	}
@@ -33,21 +42,36 @@ func SignTx(unsignedTxBase64 string) (string, error) {
 	return solanaTx.ToBase64()
 }
 
-func signTx(tx *solana.Transaction, privateKey solana.PrivateKey) error {
-	signaturesRequired := int(tx.Message.Header.NumRequiredSignatures)
-	signaturesPresent := len(tx.Signatures)
+func signTx(solanaTx *solana.Transaction, privateKey solana.PrivateKey) error {
+	signaturesRequired := int(solanaTx.Message.Header.NumRequiredSignatures)
+	signaturesPresent := len(solanaTx.Signatures)
 	if signaturesPresent != signaturesRequired-1 {
 		return fmt.Errorf("transaction requires %v signatures and has %v signatures, should need exactly one more signature", signaturesRequired, signaturesPresent)
 	}
 
-	_, err := tx.Sign(func(key solana.PublicKey) *solana.PrivateKey {
-		if key.Equals(privateKey.PublicKey()) {
-			return &privateKey
-		}
-		return nil
-	})
+	err := replaceZeroSignature(solanaTx, privateKey)
 	if err != nil {
 		return err
+	}
+	return nil
+}
+
+func replaceZeroSignature(tx *solana.Transaction, privateKey solana.PrivateKey) error {
+	messageContent, err := tx.Message.MarshalBinary()
+	if err != nil {
+		return fmt.Errorf("unable to encode message for signing: %w", err)
+	}
+
+	signedMessageContent, err := privateKey.Sign(messageContent)
+	if err != nil {
+		return fmt.Errorf("unable to sign message: %v", err)
+	}
+
+	for i, sig := range tx.Signatures {
+		if sig.IsZero() {
+			tx.Signatures[i] = signedMessageContent
+			break
+		}
 	}
 
 	return nil
