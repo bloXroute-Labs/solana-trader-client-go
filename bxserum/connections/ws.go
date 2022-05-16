138a4c587703b4027ef9289070e7cf8b88440166
Hlib Saienko <saenkogleb21@gmail.com>
2022-05-16 18:31:45 +0300

    Move all subscriptions logic to connections package

--- bxserum/connections/ws.go
+++ bxserum/connections/ws.go
@@ -1,15 +1,50 @@
 package connections
 
 import (
-	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/bloXroute-Labs/serum-api/utils"
 	"github.com/gorilla/websocket"
 	"github.com/sourcegraph/jsonrpc2"
+	"sync"
 )
 
-type SubscriptionResponse struct {
+type requests struct {
+	sync.Mutex
+	dataReceivers map[uint64]chan json.RawMessage
+}
+
+func (r *requests) add(id uint64, receiver chan json.RawMessage) {
+	r.Mutex.Lock()
+	defer r.Mutex.Unlock()
+	r.dataReceivers[id] = receiver
+}
+
+func (r *requests) delete(id uint64) {
+	r.Mutex.Lock()
+	defer r.Mutex.Unlock()
+	delete(r.dataReceivers, id)
+}
+
+type subscriptions struct {
+	*requests
+	subscriptions       map[string]chan json.RawMessage
+	subscriptionsReader chan subscriptionResponse
+}
+
+func (s *subscriptions) read() {
+	for response := range s.subscriptionsReader {
+		_, ok := s.subscriptions[response.Result]
+		if ok {
+			continue
+		}
+		s.subscriptions[response.Result] = s.dataReceivers[response.ID]
+		s.delete(response.ID)
+	}
+}
+
+type subscriptionResponse struct {
 	ID     uint64 `json:"id"`
 	Result string `json:"result"`
 }
@@ -19,117 +54,152 @@ type streamResponse struct {
 	Params         json.RawMessage `json:"params"`
 }
 
-func WSRequest[T any](conn *websocket.Conn, request []byte, responseChan chan json.RawMessage) (*T, error) {
-	err := sendWS(conn, request)
+type WSRPC struct {
+	requestID utils.RequestID
+	conn      *websocket.Conn
+
+	streamSubscriptions subscriptions
+	nonStreamRequests   requests
+}
+
+func NewWSRPC(conn *websocket.Conn) *WSRPC {
+	wsRPC := &WSRPC{
+		requestID: utils.NewRequestID(),
+		conn:      conn,
+		nonStreamRequests: requests{
+			dataReceivers: map[uint64]chan json.RawMessage{},
+		},
+		streamSubscriptions: subscriptions{
+			requests:            &requests{dataReceivers: map[uint64]chan json.RawMessage{}},
+			subscriptions:       map[string]chan json.RawMessage{},
+			subscriptionsReader: make(chan subscriptionResponse, 1000),
+		},
+	}
+
+	go wsRPC.read()
+	go wsRPC.streamSubscriptions.read()
+
+	return wsRPC
+}
+
+func (w *WSRPC) Request(methodName string, message json.RawMessage) (json.RawMessage, error) {
+	id := w.requestID.Next()
+
+	payload, err := w.makeRPCRequest(id, methodName, &message)
 	if err != nil {
 		return nil, err
 	}
-	resp := <-responseChan
 
-	var result *T
-	if err = json.Unmarshal(resp, &result); err != nil {
-		return nil, fmt.Errorf("error unmarshalling message of type %T: %v", result, err)
+	receiver := make(chan json.RawMessage)
+	w.nonStreamRequests.add(id, receiver)
+	defer w.nonStreamRequests.delete(id)
+
+	err = w.sendWS(payload)
+	if err != nil {
+		return nil, err
 	}
 
-	return result, nil
+	response := <-receiver
+
+	return response, nil
 }
 
-func WSStream[T any](ctx context.Context, conn *websocket.Conn, request []byte, subscriptionChan chan json.RawMessage, responseChan chan *T) error {
-	err := sendWS(conn, request)
+func (w *WSRPC) Stream(methodName string, message json.RawMessage, responses chan json.RawMessage) error {
+	id := w.requestID.Next()
+
+	payload, err := w.makeRPCRequest(id, methodName, &message)
 	if err != nil {
 		return err
 	}
+	w.streamSubscriptions.add(id, responses)
+	return w.sendWS(payload)
+}
 
-	go func(responseChan chan *T, conn *websocket.Conn) {
-		for {
-			select {
-			case <-ctx.Done():
-				return
-			case resp := <-subscriptionChan:
-				var result T
-				if err = json.Unmarshal(resp, &result); err != nil {
-					return
-				}
-				responseChan <- &result
-			}
-		}
-	}(responseChan, conn)
-
+func (w *WSRPC) Close() error {
+	err := w.conn.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+	if err != nil {
+		return fmt.Errorf("error writing close msg -  %v", err)
+	}
 	return nil
 }
 
-func sendWS(conn *websocket.Conn, request []byte) error {
-	if err := conn.WriteMessage(websocket.TextMessage, request); err != nil {
+func (w *WSRPC) makeRPCRequest(requestID uint64, methodName string, message *json.RawMessage) ([]byte, error) {
+	rpcMessage := jsonrpc2.Request{ID: jsonrpc2.ID{Num: requestID}, Method: methodName, Params: message}
+	return rpcMessage.MarshalJSON()
+}
+
+func (w *WSRPC) sendWS(request []byte) error {
+	if err := w.conn.WriteMessage(websocket.TextMessage, request); err != nil {
 		return fmt.Errorf("error with sending message: %v", err)
 	}
 	return nil
 }
 
-func RecvWS(conn *websocket.Conn, nonStreamResponsesReaders map[uint64]chan json.RawMessage, streamResponsesReaders map[string]chan json.RawMessage, subscriptionsReader chan SubscriptionResponse) error {
-	_, msg, err := conn.ReadMessage()
-	if err != nil {
-		return fmt.Errorf("error reading WS response: %v", err)
-	}
+func (w *WSRPC) read() error {
+	for {
+		_, msg, err := w.conn.ReadMessage()
+		if err != nil {
+			return fmt.Errorf("error reading WS response: %v", err)
+		}
 
-	resp := jsonrpc2.Response{}
+		resp := jsonrpc2.Response{}
 
-	if err = json.Unmarshal(msg, &resp); err != nil {
-		return fmt.Errorf("error unmarshalling JSON response: %v", err)
-	}
+		if err = json.Unmarshal(msg, &resp); err != nil {
+			return fmt.Errorf("error unmarshalling JSON response: %v", err)
+		}
 
-	if resp.Error != nil {
-		if resp.Error.Data != nil {
-			m, err := json.Marshal(resp.Error.Data)
+		if resp.Error != nil {
+			if resp.Error.Data != nil {
+				m, err := json.Marshal(resp.Error.Data)
+				if err != nil {
+					return err
+				}
+
+				return errors.New(string(m))
+			}
+
+			return errors.New("failed to check ")
+		}
+
+		if resp.ID.Str == "null" {
+			subscription := streamResponse{}
+			err := json.Unmarshal(*resp.Result, &subscription)
 			if err != nil {
 				return err
 			}
 
-			return errors.New(string(m))
+			if subscription.SubscriptionID == "" {
+				return errors.New("received invalid subscription ID")
+			}
+
+			responseReader, ok := w.streamSubscriptions.subscriptions[subscription.SubscriptionID]
+			if !ok {
+				return nil
+			}
+
+			responseReader <- subscription.Params
+
+			continue
 		}
 
-		return errors.New("failed to check ")
-	}
-
-	if resp.ID.Str == "null" {
-		subscription := streamResponse{}
-		err := json.Unmarshal(*resp.Result, &subscription)
-		if err != nil {
-			return err
+		if resp.ID.IsString {
+			return errors.New("received invalid request ID")
 		}
 
-		if subscription.SubscriptionID == "" {
-			return errors.New("received invalid subscription ID")
+		// new subscription for feed
+		subscriptionResponse := subscriptionResponse{}
+		err = json.Unmarshal(msg, &subscriptionResponse)
+		if err == nil {
+			subscriptionResponse.ID = resp.ID.Num
+			w.streamSubscriptions.subscriptionsReader <- subscriptionResponse
+			continue
 		}
 
-		responseReader, ok := streamResponsesReaders[subscription.SubscriptionID]
+		responseReader, ok := w.nonStreamRequests.dataReceivers[resp.ID.Num]
 		if !ok {
-			return nil
+			continue
 		}
 
-		responseReader <- subscription.Params
-
-		return nil
+		responseReader <- *resp.Result
 	}
-
-	if resp.ID.IsString {
-		return errors.New("received invalid request ID")
-	}
-
-	// new subscription for feed
-	subscriptionResponse := SubscriptionResponse{}
-	err = json.Unmarshal(msg, &subscriptionResponse)
-	if err == nil {
-		subscriptionResponse.ID = resp.ID.Num
-		subscriptionsReader <- subscriptionResponse
-		return nil
-	}
-
-	responseReader, ok := nonStreamResponsesReaders[resp.ID.Num]
-	if !ok {
-		return nil
-	}
-
-	responseReader <- *resp.Result
-
-	return nil
 }
--- bxserum/provider/ws.go
+++ bxserum/provider/ws.go
@@ -3,59 +3,21 @@ package provider
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"github.com/bloXroute-Labs/serum-api/bxserum/connections"
 	pb "github.com/bloXroute-Labs/serum-api/proto"
-	"github.com/bloXroute-Labs/serum-api/utils"
 	"github.com/gorilla/websocket"
-	"github.com/sourcegraph/jsonrpc2"
-	"log"
-	"sync"
 )
 
-type requests struct {
-	sync.Mutex
-	dataReceivers map[uint64]chan json.RawMessage
-}
-
-func (r *requests) add(id uint64, receiver chan json.RawMessage) {
-	r.Mutex.Lock()
-	defer r.Mutex.Unlock()
-	r.dataReceivers[id] = receiver
-}
-
-func (r *requests) delete(id uint64) {
-	r.Mutex.Lock()
-	defer r.Mutex.Unlock()
-	delete(r.dataReceivers, id)
-}
-
-type subscriptions struct {
-	*requests
-	subscriptions       map[string]chan json.RawMessage
-	subscriptionsReader chan connections.SubscriptionResponse
-}
-
-func (s *subscriptions) read() {
-	for response := range s.subscriptionsReader {
-		log.Println(response)
-		_, ok := s.subscriptions[response.Result]
-		if ok {
-			continue
-		}
-		s.subscriptions[response.Result] = s.dataReceivers[response.ID]
-		s.delete(response.ID)
-	}
+type WsRPC interface {
+	Request(methodName string, message json.RawMessage) (json.RawMessage, error)
+	Stream(methodName string, message json.RawMessage, responses chan json.RawMessage) error
+	Close() error
 }
 
 type WSClient struct {
 	pb.UnimplementedApiServer
 
-	addr                string
-	conn                *websocket.Conn
-	requestID           utils.RequestID
-	requests            requests
-	streamSubscriptions subscriptions
+	connection WsRPC
 }
 
 // NewWSClient connects to Mainnet Serum API
@@ -76,95 +38,76 @@ func NewWSClientWithEndpoint(addr string) (*WSClient, error) {
 	}
 
 	client := &WSClient{
-		addr:      addr,
-		conn:      conn,
-		requestID: utils.NewRequestID(),
-		requests: requests{
-			dataReceivers: map[uint64]chan json.RawMessage{},
-		},
-		streamSubscriptions: subscriptions{
-			requests:            &requests{dataReceivers: map[uint64]chan json.RawMessage{}},
-			subscriptions:       map[string]chan json.RawMessage{},
-			subscriptionsReader: make(chan connections.SubscriptionResponse, 1000),
-		},
+		connection: connections.NewWSRPC(conn),
 	}
-
-	go client.streamSubscriptions.read()
-	go client.read()
 
 	return client, nil
 }
 
 // Set limit to 0 to get all bids/asks
 func (w *WSClient) GetOrderbook(market string, limit uint32) (*pb.GetOrderbookResponse, error) {
-	request, requestID, err := w.jsonRPCRequest("GetOrderbook", map[string]interface{}{"market": market, "limit": limit})
+	message, err := json.Marshal(map[string]interface{}{"market": market, "limit": limit})
 	if err != nil {
 		return nil, err
 	}
 
-	responseChan := make(chan json.RawMessage)
-	w.requests.add(requestID, responseChan)
-	defer w.requests.delete(requestID)
+	result, err := w.connection.Request("GetOrderbook", message)
+	if err != nil {
+		return nil, err
+	}
 
-	return connections.WSRequest[pb.GetOrderbookResponse](w.conn, request, responseChan)
+	return unmarshalJSON[pb.GetOrderbookResponse](result)
 }
 
 func (w *WSClient) GetOrderbookStream(ctx context.Context, market string, limit uint32, orderbookChan chan *pb.GetOrderbookStreamResponse) error {
-	request, requestID, err := w.jsonRPCRequest("GetOrderbookStream", map[string]interface{}{"market": market, "limit": limit})
+	message, err := json.Marshal(map[string]interface{}{"market": market, "limit": limit})
 	if err != nil {
 		return err
 	}
 
-	responseChan := make(chan json.RawMessage)
-	w.streamSubscriptions.add(requestID, responseChan)
+	response := make(chan json.RawMessage)
 
-	return connections.WSStream[pb.GetOrderbookStreamResponse](ctx, w.conn, request, responseChan, orderbookChan)
+	err = w.connection.Stream("GetOrderbookStream", message, response)
+	if err != nil {
+		return err
+	}
+
+	go readStreamResponse[pb.GetOrderbookStreamResponse](ctx, response, orderbookChan)
+
+	return nil
 }
 
 func (w *WSClient) GetMarkets() (*pb.GetMarketsResponse, error) {
-	request, requestID, err := w.jsonRPCRequest("GetMarkets", nil)
+	result, err := w.connection.Request("GetMarkets", nil)
 	if err != nil {
 		return nil, err
 	}
 
-	responseChan := make(chan json.RawMessage)
-	w.requests.add(requestID, responseChan)
-	defer w.requests.delete(requestID)
-
-	return connections.WSRequest[pb.GetMarketsResponse](w.conn, request, responseChan)
+	return unmarshalJSON[pb.GetMarketsResponse](result)
 }
 
 // TODO: Add Unsubscribe for each subscription
 func (w *WSClient) Close() error {
-	err := w.conn.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
-	if err != nil {
-		return fmt.Errorf("error writing close msg -  %v", err)
-	}
-	return nil
+	return w.connection.Close()
 }
 
-func (w *WSClient) jsonRPCRequest(method string, params map[string]interface{}) ([]byte, uint64, error) {
-	id := w.requestID.Next()
-	req := jsonrpc2.Request{
-		Method: method,
-		ID:     jsonrpc2.ID{Num: id},
-	}
-	if err := req.SetParams(params); err != nil {
-		return nil, 0, err
-	}
-	payload, err := req.MarshalJSON()
-	if err != nil {
-		return nil, 0, err
-	}
-
-	return payload, id, nil
-}
-
-func (w *WSClient) read() {
+func readStreamResponse[T any](ctx context.Context, streamChan chan json.RawMessage, clientChan chan *T) {
 	for {
-		err := connections.RecvWS(w.conn, w.requests.dataReceivers, w.streamSubscriptions.subscriptions, w.streamSubscriptions.subscriptionsReader)
-		if err != nil {
-			break
+		select {
+		case <-ctx.Done():
+			return
+		case rawMessage := <-streamChan:
+			result, err := unmarshalJSON[T](rawMessage)
+			if err != nil {
+				continue
+			}
+			clientChan <- result
 		}
 	}
+}
+
+func unmarshalJSON[T any](message json.RawMessage) (*T, error) {
+	var result T
+	err := json.Unmarshal(message, &result)
+	return &result, err
 }
