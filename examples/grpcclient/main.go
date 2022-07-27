eb4d97bf587a504cd6d130031b7a201433e4b079
Kevin Chen <kevin@bloxroute.com>
2022-07-27 16:23:46 -0500

    TRAD-96 update grpc example

--- examples/grpcclient/main.go
+++ examples/grpcclient/main.go
@@ -152,14 +152,21 @@ func callOrderbookGRPCStream(g *provider.GRPCClient) {
 	defer cancel()
 
 	// Stream response
-	err := g.GetOrderbookStream(ctx, []string{"SOL/USDC", "xxx", "SOL-USDT"}, 3, orderbookChan)
+	stream, err := g.GetOrderbookStream(ctx, []string{"SOL/USDC", "xxx", "SOL-USDT"}, 3)
 	if err != nil {
 		log.Errorf("error with GetOrderbook stream request for SOL/USDC: %v", err)
-	} else {
-		for i := 1; i <= 5; i++ {
-			data := <-orderbookChan
-			fmt.Printf("response %v received, data %v \n", i, data)
+		return
+	}
+
+	stream.Into(orderbookChan)
+	for i := 1; i <= 5; i++ {
+		data, ok := <-orderbookChan
+		if !ok {
+			// channel closed
+			return
 		}
+
+		fmt.Printf("response %v received, data %v \n", i, data)
 	}
 }
 
@@ -171,14 +178,18 @@ func callTradesGRPCStream(g *provider.GRPCClient) {
 	defer cancel()
 
 	// Stream response
-	err := g.GetTradesStream(ctx, "SOL/USDC", 3, tradesChan)
+	stream, err := g.GetTradesStream(ctx, "SOL/USDC", 3)
 	if err != nil {
 		log.Errorf("error with GetTrades stream request for SOL/USDC: %v", err)
-	} else {
-		for i := 1; i <= 3; i++ {
-			<-tradesChan
-			fmt.Printf("response %v received\n", i)
+	}
+	stream.Into(tradesChan)
+	for i := 1; i <= 3; i++ {
+		_, ok := <-tradesChan
+		if !ok {
+			// channel closed
+			return
 		}
+		fmt.Printf("response %v received\n", i)
 	}
 }
 
@@ -201,10 +212,11 @@ func orderLifecycleTest(g *provider.GRPCClient, ownerAddr string, ooAddr string)
 
 	ch := make(chan *pb.GetOrderStatusStreamResponse)
 	go func() {
-		err := g.GetOrderStatusStream(ctx, marketAddr, ownerAddr, ch)
+		stream, err := g.GetOrderStatusStream(ctx, marketAddr, ownerAddr)
 		if err != nil {
 			log.Fatalf("error getting order status stream %v", err)
 		}
+		stream.Into(ch)
 	}()
 
 	time.Sleep(time.Second * 10)
