85c6d712730d5d2843f4e8fa75dd46f4355db17d
Kevin Chen <kevin@bloxroute.com>
2022-08-02 16:00:14 -0500

    TRAD-108 add Dockerfile for building

--- benchmark/internal/utils/flag.go
+++ benchmark/internal/utils/flag.go
@@ -11,11 +11,9 @@ var (
 	SolanaHTTPRPCEndpointFlag = &cli.StringFlag{
 		Name:  "solana-http-endpoint",
 		Usage: "HTTP RPC server endpoint to make blockchain queries against",
-		Value: "http://34.203.186.197:8899",
 	}
 	SolanaWSRPCEndpointFlag = &cli.StringFlag{
 		Name:  "solana-ws-endpoint",
 		Usage: "WS RPC server endpoint to make blockchain pub/sub queries against",
-		Value: "ws://34.203.186.197:8900/ws",
 	}
 )
