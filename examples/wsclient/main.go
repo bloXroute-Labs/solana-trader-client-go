5843860b9b927459599de90a499bcfe0568147ce
Kevin Chen <kevin@bloxroute.com>
2023-01-31 16:49:04 -0600

    TRAD-525 remove ray for now (#87)

--- examples/wsclient/main.go
+++ examples/wsclient/main.go
@@ -888,17 +888,23 @@ func callRouteTradeSwap(w *provider.WSClient, ownerAddr string) bool {
 		Project:      pb.Project_P_RAYDIUM,
 		Steps: []*pb.RouteStep{
 			{
-				// FIDA-RAY pool address
+				Project: &pb.StepProject{
+					Label: "Raydium",
+					Id:    "",
+				},
 				InToken:  "FIDA",
-				OutToken: "RAY",
+				OutToken: "4k3Dyjzvzp8eMZWUXbBCjEvwSkkk59S5iCNLY3QrkX6R",
 
 				InAmount:     0.01,
 				OutAmountMin: 0.007505,
 				OutAmount:    0.0074,
 			},
 			{
-				// RAY-USDC pool address
-				InToken:      "RAY",
+				Project: &pb.StepProject{
+					Label: "Raydium",
+					Id:    "",
+				},
+				InToken:      "4k3Dyjzvzp8eMZWUXbBCjEvwSkkk59S5iCNLY3QrkX6R",
 				OutToken:     "USDC",
 				InAmount:     0.007505,
 				OutAmount:    0.004043,
