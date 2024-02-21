0770f51e79bbcd723c7e94c1f181a34eeb9af9ca
atulsriv <atul.srivastava@bloxroute.com>
2024-02-21 14:03:43 -0500

    add utils

--- utils/proto.go
+++ utils/proto.go
@@ -1,1 +1,58 @@
 package utils
+
+import (
+	pb "github.com/bloXroute-Labs/solana-trader-proto/api"
+	"github.com/gagliardetto/solana-go"
+)
+
+func ConvertProtoAddressLookupTable(addressLookupTableProto map[string]*pb.PublicKeys) (map[solana.PublicKey]solana.PublicKeySlice, error) {
+	addressLookupTable := make(map[solana.PublicKey]solana.PublicKeySlice)
+
+	for pk, accounts := range addressLookupTableProto {
+		solanaPk, err := solana.PublicKeyFromBase58(pk)
+		if err != nil {
+			return nil, err
+		}
+
+		var solanaPkSlice solana.PublicKeySlice
+
+		for _, acc := range accounts.Pks {
+			accPk, err := solana.PublicKeyFromBase58(acc)
+			if err != nil {
+				return nil, err
+			}
+
+			solanaPkSlice = append(solanaPkSlice, accPk)
+		}
+
+		addressLookupTable[solanaPk] = solanaPkSlice
+	}
+
+	return addressLookupTable, nil
+}
+
+func ConvertProtoInstructionsToSolanaInstructions(instructions []*pb.InstructionJupiter) ([]solana.Instruction, error) {
+	var solanaInstructions []solana.Instruction
+
+	for _, inst := range instructions {
+		programID, err := solana.PublicKeyFromBase58(inst.ProgramID)
+		if err != nil {
+			return nil, err
+		}
+
+		var accountMetaSlice solana.AccountMetaSlice
+
+		for _, acc := range inst.Accounts {
+			programID, err := solana.PublicKeyFromBase58(acc.ProgramID)
+			if err != nil {
+				return nil, err
+			}
+			accountMetaSlice = append(accountMetaSlice, solana.NewAccountMeta(
+				programID, acc.IsWritable, acc.IsSigner))
+		}
+
+		solanaInstructions = append(solanaInstructions, solana.NewInstruction(programID, accountMetaSlice, inst.Data))
+	}
+
+	return solanaInstructions, nil
+}
