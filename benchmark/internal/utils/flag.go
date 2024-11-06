e35d4cdf719998e34fb30527242717aa32527f7e
Mahmoud Taabodi <mahmoud.taabodi@bloxroute.com>
2024-11-06 16:17:02 -0500

    fixing

--- benchmark/internal/utils/flag.go
+++ benchmark/internal/utils/flag.go
@@ -8,4 +8,17 @@ var (
 		Usage:    "file to output CSV results to",
 		Required: true,
 	}
+	SolanaHTTPRPCEndpointFlag = &cli.StringFlag{
+		Name:  "solana-http-endpoint",
+		Usage: "HTTP RPC server endpoint to make blockchain queries against",
+	}
+	SolanaWSRPCEndpointFlag = &cli.StringFlag{
+		Name:  "solana-ws-endpoint",
+		Usage: "WS RPC server endpoint to make blockchain pub/sub queries against",
+	}
+	APIWSEndpoint = &cli.StringFlag{
+		Name:  "solana-trader-ws-endpoint",
+		Usage: "Solana Trader API API websocket connection endpoint",
+		Value: "wss://virginia.solana.dex.blxrbdn.com/ws",
+	}
 )
