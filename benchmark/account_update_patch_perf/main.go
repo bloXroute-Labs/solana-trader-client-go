98e06f0938e025b395956df4fbf48d554c3076f5
808putnam <wmcroberts3@gmail.com>
2024-12-15 09:38:20 -0500

    chore: better formatting

--- benchmark/account_update_patch_perf/main.go
+++ benchmark/account_update_patch_perf/main.go
@@ -5,76 +5,71 @@ import (
 	"crypto/sha256"
 	"flag"
 	"fmt"
+	"github.com/bloXroute-Labs/solana-trader-client-go/provider"
+	pb "github.com/bloXroute-Labs/solana-trader-proto/api"
 	"math"
 	"sort"
 	"strconv"
 	"strings"
 	"sync"
 	"time"
-	"github.com/bloXroute-Labs/solana-trader-client-go/provider"
-	pb "github.com/bloXroute-Labs/solana-trader-proto/api"
 )
 
 var runDuration int
-var idleBeforeStart int
 
 func createHash(update *pb.GetNewRaydiumPoolsResponse) string {
-	data := fmt.Sprintf("%d%s%s%d%s%s%d%s%s%d%s%s%s%s%s%d%s%s%s%s%s%s%s%s%d%s%s%s%s%s%s%s%s%d", 
+	data := fmt.Sprintf("%d%s%s%d%s%s%d%s%s%d%s%s%s%s%s%d%s%s%s%s%s%s%s%s%d%s%s%s%s%s%s%s%s%d",
 		update.Slot,
-		update.Pool.Pool,          
-		update.Pool.PoolAddress,   
-		update.Pool.Token1Reserves,   
+		update.Pool.Pool,
+		update.Pool.PoolAddress,
+		update.Pool.Token1Reserves,
 		update.Pool.Token1MintAddress,
 		update.Pool.Token1MintSymbol,
 		update.Pool.Token2Reserves,
 		update.Pool.Token2MintAddress,
-		update.Pool.Token2MintSymbol, 
-		update.Pool.OpenTime,         
+		update.Pool.Token2MintSymbol,
+		update.Pool.OpenTime,
 		update.Pool.PoolType,
-		update.Pool.LiquidityPoolKeys.Id,              
-		update.Pool.LiquidityPoolKeys.BaseMint,        
-		update.Pool.LiquidityPoolKeys.QuoteMint,       
-		update.Pool.LiquidityPoolKeys.LpMint,          
-		update.Pool.LiquidityPoolKeys.Version,         
-		update.Pool.LiquidityPoolKeys.ProgramID,       
-		update.Pool.LiquidityPoolKeys.Authority,       
-		update.Pool.LiquidityPoolKeys.BaseVault,       
-		update.Pool.LiquidityPoolKeys.QuoteVault,      
-		update.Pool.LiquidityPoolKeys.LpVault,         
-		update.Pool.LiquidityPoolKeys.OpenOrders,      
-		update.Pool.LiquidityPoolKeys.TargetOrders,    
-		update.Pool.LiquidityPoolKeys.WithdrawQueue,   
-		update.Pool.LiquidityPoolKeys.MarketVersion,   
-		update.Pool.LiquidityPoolKeys.MarketProgramID, 
-		update.Pool.LiquidityPoolKeys.MarketID,        
-		update.Pool.LiquidityPoolKeys.MarketAuthority, 
-		update.Pool.LiquidityPoolKeys.MarketBaseVault, 
+		update.Pool.LiquidityPoolKeys.Id,
+		update.Pool.LiquidityPoolKeys.BaseMint,
+		update.Pool.LiquidityPoolKeys.QuoteMint,
+		update.Pool.LiquidityPoolKeys.LpMint,
+		update.Pool.LiquidityPoolKeys.Version,
+		update.Pool.LiquidityPoolKeys.ProgramID,
+		update.Pool.LiquidityPoolKeys.Authority,
+		update.Pool.LiquidityPoolKeys.BaseVault,
+		update.Pool.LiquidityPoolKeys.QuoteVault,
+		update.Pool.LiquidityPoolKeys.LpVault,
+		update.Pool.LiquidityPoolKeys.OpenOrders,
+		update.Pool.LiquidityPoolKeys.TargetOrders,
+		update.Pool.LiquidityPoolKeys.WithdrawQueue,
+		update.Pool.LiquidityPoolKeys.MarketVersion,
+		update.Pool.LiquidityPoolKeys.MarketProgramID,
+		update.Pool.LiquidityPoolKeys.MarketID,
+		update.Pool.LiquidityPoolKeys.MarketAuthority,
+		update.Pool.LiquidityPoolKeys.MarketBaseVault,
 		update.Pool.LiquidityPoolKeys.MarketQuoteVault,
-		update.Pool.LiquidityPoolKeys.MarketBids,      
-		update.Pool.LiquidityPoolKeys.MarketAsks,      
+		update.Pool.LiquidityPoolKeys.MarketBids,
+		update.Pool.LiquidityPoolKeys.MarketAsks,
 		update.Pool.LiquidityPoolKeys.MarketEventQueue,
-		update.Pool.LiquidityPoolKeys.TradeFeeRate,    
+		update.Pool.LiquidityPoolKeys.TradeFeeRate,
 	)
-	return fmt.Sprintf("%x%s", sha256.Sum256([]byte(data)), data)
+	// return fmt.Sprintf("%x%s", sha256.Sum256([]byte(data)), data)
+	return fmt.Sprintf("%x", sha256.Sum256([]byte(data)))
 }
 
 func main() {
 
 	flag.IntVar(&runDuration, "duration", 1, "run duration in minutes")
-	flag.IntVar(&idleBeforeStart, "idle-before-start", 1, "how much to wait before analyzing the data")
 	flag.Parse()
 
 	var ch = make(chan accountUpdate, 1e7)
 	var wg = new(sync.WaitGroup)
-	var ctx, _ = context.WithTimeout(context.Background(), time.Duration(runDuration)*time.Minute+time.Duration(idleBeforeStart)*time.Minute)
-
-	var idleEnd = time.Now().Add(time.Duration(idleBeforeStart) * time.Minute)
-
-	fmt.Println("start idle, end at:", idleEnd.Format(time.TimeOnly))
+	var ctx, _ = context.WithTimeout(context.Background(), time.Duration(runDuration)*time.Minute)
 
 	wg.Add(2)
-	go accountUpdatesPatchLib(ctx, wg, ch, "127.0.0.1:9000", sourceAccountUpdateStream, idleEnd, false)
-	go accountUpdatesPatchLib(ctx, wg, ch, "127.0.0.1:9001", sourceAccountUpdatePatchStream, idleEnd, false)
+	go accountUpdatesPatchLib(ctx, wg, ch, "127.0.0.1:9001", sourceAccountUpdateStream, false)
+	go accountUpdatesPatchLib(ctx, wg, ch, "127.0.0.1:9000", sourceAccountUpdatePatchStream, false)
 
 	wg.Wait()
 	close(ch)
@@ -83,7 +78,7 @@ func main() {
 	var timeDiffsUs = make([]int64, 0)
 
 	for upd := range ch {
-		
+
 		hash := createHash(upd.update)
 
 		if v, ok := mp[hash]; !ok {
@@ -95,7 +90,6 @@ func main() {
 			}
 
 			diff := upd.rcvTime.Sub(v.rcvTime).Microseconds()
-			// TODO: confirm logic for diff, but we are mainly interested in content compare
 			if upd.source == sourceAccountUpdateStream { // VANILLA WIN
 				// sourceAccountUpdatePatchStream is WINNER (-)
 				timeDiffsUs = append(timeDiffsUs, -diff)
@@ -108,19 +102,44 @@ func main() {
 		}
 	}
 
-	fmt.Println("calculating histogram")
+	// Did we have pool events with no match?
+	streamCount := 0
+	patchStreamCount := 0
+	for hash, entry := range mp {
+		fmt.Printf("Hash: %s, Entry: %+v\n", hash, entry)
+		if entry.source == sourceAccountUpdateStream {
+			streamCount++
+		} else if entry.source == sourceAccountUpdatePatchStream {
+			patchStreamCount++
+		}
+	}
+
+	fmt.Println("----------------")
+	fmt.Println("Benchmark comparing contents of AccountUpdateStream and AccountUpdatePatchStream")
+	fmt.Println("- sdk run for GetNewRaydiumPoolsStream")
+	fmt.Println("Duration: ", runDuration, "minutes")
+	fmt.Println("----------------")
+	fmt.Printf("Entries from AccountUpdateStream with no match: %d\n", streamCount)
+	fmt.Printf("Entries from AccountUpdatePatchStream with no match: %d\n", patchStreamCount)
 
 	timeDiffsMs := []float64{}
 	for _, timestamp := range timeDiffsUs {
 		timeDiffsMs = append(timeDiffsMs, float64(timestamp)/1000)
 	}
 
-	fmt.Println("compare pairs:", len(timeDiffsUs))
+	fmt.Println("----------------")
+	fmt.Println("Number of pairs that matched:", len(timeDiffsUs))
 
 	fmt.Println("----------------")
-	fmt.Println("values with (+) is AccountUpdatesStream, values with (-) is AccountUpdatesPatchStream")
-	fmt.Println(calculatePercentiles(timeDiffsUs))
+	fmt.Println("values with (+) is AccountUpdatesStream as the winner")
+	fmt.Println("values with (-) is AccountUpdatesPatchStream as the winner")
+	calculatePercentiles(timeDiffsUs)
+
+	fmt.Println("----------------")
+	fmt.Println("Grouping by millisecond")
 	fmt.Println(makeHistogram(timeDiffsMs))
+	fmt.Println("----------------")
+	fmt.Println("")
 }
 
 const (
@@ -134,11 +153,9 @@ type accountUpdate struct {
 	source    source
 	rcvTime   time.Time
 	update    *pb.GetNewRaydiumPoolsResponse
-	batchUid  string
-	batchSize int
 }
 
-func accountUpdatesPatchLib(ctx context.Context, wg *sync.WaitGroup, ch chan accountUpdate, addr string, src source, idleEnd time.Time, testnet bool) {
+func accountUpdatesPatchLib(ctx context.Context, wg *sync.WaitGroup, ch chan accountUpdate, addr string, src source, testnet bool) {
 	defer wg.Done()
 
 	var client *provider.GRPCClient
@@ -148,13 +165,13 @@ func accountUpdatesPatchLib(ctx context.Context, wg *sync.WaitGroup, ch chan acc
 		if err != nil {
 			panic("failed to get grpc client")
 		}
-	
+
 	} else {
 		opts := provider.DefaultRPCOpts(addr)
 		client, err = provider.NewGRPCClientWithOpts(opts)
 		if err != nil {
 			panic("failed to get grpc client")
-		}	
+		}
 	}
 
 	chRaydium := make(chan *pb.GetNewRaydiumPoolsResponse)
@@ -169,14 +186,12 @@ func accountUpdatesPatchLib(ctx context.Context, wg *sync.WaitGroup, ch chan acc
 		case <-ctx.Done():
 			return
 		case pool := <-chRaydium:
-			fmt.Println("New Raydium pool:", pool)
+			fmt.Println("New Raydium pool:", pool.Pool.PoolAddress)
 			select {
 			case ch <- accountUpdate{
 				source:    src,
 				rcvTime:   time.Now(),
 				update:    pool,
-				batchUid:  "",
-				batchSize: 0,
 			}:
 			default:
 				panic("accountUpdatesPatchLib: send into recv chan failed: full")
@@ -253,26 +268,23 @@ func getBinEnd(binIndex int) string {
 	if binIndex == numBins-1 {
 		return "+∞"
 	}
-	
+
 	return fmt.Sprintf("%d", -10+binIndex*binSize)
 }
 
 var percentiles = []int{10, 25, 50, 75, 90}
 
-func calculatePercentiles(data []int64) string {
+func calculatePercentiles(data []int64) {
 	if len(data) == 0 {
-		return ""
+		return
 	}
 	sort.Slice(data, func(i, j int) bool {
 		return data[i] < data[j]
 	})
 
-	result := ""
 	for _, percentile := range percentiles {
-		result += fmt.Sprintf("P%d: %dμs ", percentile, calculatePercentile(data, float64(percentile)))
+		fmt.Printf("P%d: %dμs\n", percentile, calculatePercentile(data, float64(percentile)))
 	}
-
-	return result
 }
 
 func calculatePercentile(data []int64, percentile float64) int64 {
